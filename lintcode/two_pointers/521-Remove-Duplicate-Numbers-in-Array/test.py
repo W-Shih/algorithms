0bbdec999af59480160af8b4d8f9edaee2c34ab1
wen-wayne-shih <wen.shih@gapp.nthu.edu.tw>
2022-09-21 19:50:07 -0700

    lintcode: 521-Remove-Duplicate-Numbers-in-Array-by-Hash
    resolve: resolve review comments

--- lintcode/two_pointers/521-Remove-Duplicate-Numbers-in-Array/test.py
+++ lintcode/two_pointers/521-Remove-Duplicate-Numbers-in-Array/test.py
@@ -7,6 +7,7 @@
 # =================================================================================================
 #    Date      Name                    Description of Change
 # 19-Nov-2021  Wayne Shih              Initial create
+# 21-Sep-2022  Wayne Shih              Add test for another solution by hash
 # $HISTORY$
 # =================================================================================================
 
@@ -19,19 +20,39 @@ from .solution import Solution
 class TestSolution(unittest.TestCase):
 
     def setUp(self):
-        self.test_method = Solution().deduplication
+        self.test_method_1 = Solution().deduplication_by_ptrs
+        self.test_method_2 = Solution().deduplication_by_hash
 
     def test_solution(self):
-        result = self.test_method([4, 5, 1, 2, 3])
+        # <Wayne Shih> 21-Sep-2022
+        # Test deduplication_by_ptrs()
+        result = self.test_method_1([4, 5, 1, 2, 3])
         self.assertEqual(result, 5)
-        result = self.test_method([4, 1, 1, 2, 3])
+        result = self.test_method_1([4, 1, 1, 2, 3])
         self.assertEqual(result, 4)
-        result = self.test_method([4, 1, 1, 1, 1])
+        result = self.test_method_1([4, 1, 1, 1, 1])
         self.assertEqual(result, 2)
-        result = self.test_method([1, 1, 1, 1])
+        result = self.test_method_1([1, 1, 1, 1])
         self.assertEqual(result, 1)
 
-        result = self.test_method([])
+        result = self.test_method_1([])
         self.assertEqual(result, 0)
-        result = self.test_method(None)
+        result = self.test_method_1(None)
         self.assertEqual(result, 0)
+
+        # <Wayne Shih> 21-Sep-2022
+        # Test deduplication_by_hash()
+        result = self.test_method_2([4, 5, 1, 2, 3])
+        self.assertEqual(result, 5)
+        result = self.test_method_2([4, 1, 1, 2, 3])
+        self.assertEqual(result, 4)
+        result = self.test_method_2([4, 1, 1, 1, 1])
+        self.assertEqual(result, 2)
+        result = self.test_method_2([1, 1, 1, 1])
+        self.assertEqual(result, 1)
+
+        result = self.test_method_2([])
+        self.assertEqual(result, 0)
+        result = self.test_method_2(None)
+        self.assertEqual(result, 0)
+
